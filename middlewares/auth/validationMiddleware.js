af2c4ad2c2f3bc291f5a635d4a93df0d222d5adf
CaMi <jeanpaulcami@live.com>
2024-08-08 17:39:16 +0200

    Add auth validation

---
+++ middlewares/auth/validationMiddleware.js
@@ -0,0 +1,30 @@
+// middlewares/validationMiddleware.js
+const { body, validationResult } = require('express-validator');
+
+// Validation rules for user registration
+const registerValidation = [
+    body('name').notEmpty().withMessage('Name is required'),
+    body('email').isEmail().withMessage('Invalid email format'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+];
+
+// Validation rules for user login
+const loginValidation = [
+    body('email').isEmail().withMessage('Invalid email format'),
+    body('password').notEmpty().withMessage('Password is required'),
+];
+
+// Middleware to handle validation errors
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+};
+
+module.exports = {
+    registerValidation,
+    loginValidation,
+    validate,
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 // routes/authRoutes.js
 const express = require('express');
 const { registerUser, loginUser } = require('../controllers/authController');
+const {registerValidation, validate, loginValidation} = require("../middlewares/auth/validationMiddleware");
 
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/register',registerValidation,validate, registerUser);
+router.post('/login',loginValidation,validate, loginUser);
 
 module.exports = router;
