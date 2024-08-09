cc1b075cd7b2f2a1f002b6272a752b6e23e25ec1
CaMi <jeanpaulcami@live.com>
2024-08-09 15:49:49 +0200

    start authentication pages

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/api/transactions', transactionRoutes);
 app.use('/api/budgets', budgetRoutes);
 app.use('/api/insights', insightsRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
